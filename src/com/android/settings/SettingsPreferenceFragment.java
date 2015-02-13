2b43ab80e0249de58a0e48693ae74fc835b9ad8d
blb <blb@rock-chips.com>
2015-02-13 19:40:25 +0800

    add ethernet settings

--- src/com/android/settings/SettingsPreferenceFragment.java
+++ src/com/android/settings/SettingsPreferenceFragment.java
@@ -338,6 +338,7 @@ public class SettingsPreferenceFragment extends PreferenceFragment implements Di
         }
         mDialogFragment = new SettingsDialogFragment(this, dialogId);
         mDialogFragment.show(getChildFragmentManager(), Integer.toString(dialogId));
+        Log.e("blb","----------------------------------------------------------------------------------"+getChildFragmentManager());
     }
 
     public Dialog onCreateDialog(int dialogId) {
